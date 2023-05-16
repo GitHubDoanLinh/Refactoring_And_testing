fe0e03c7cf0009e3ccd01841b49d095a49812812
GitHubDoanLinh <68548484+GitHubDoanLinh@users.noreply.github.com>
2023-05-16 22:14:11 +0700

    .
    .

--- src/triangle/TriangleClassifier.java
+++ src/triangle/TriangleClassifier.java
@@ -1,2 +1,27 @@
-package triangle;public class TriangleClassifier {
+package triangle;
+
+public class TriangleClassifier {
+    public static final String TAM_GIAC_DEU = "Tam Giac Deu";
+    public static final String TAMGIACCAN = "Tam Giac Can";
+    public static final String TAMGIACTHUONG = "Tam Giac Thuong";
+    public static final String NOTATRIANGLE = "Khong Phai Tam Giac";
+
+
+
+    public static String classify(int a, int b, int c){
+        if(a==b && b==c &&(a+b>c && b+c>a && a+c>b) && (a>0 && b>0 && c>0))
+            return TAM_GIAC_DEU;
+        if((a==b || a==c || b==c) && (a+b>c && b+c>a && a+c>b) && (a>0 && b>0 && c>0))
+            return TAMGIACCAN;
+        if(a!=b && b!=c && c!=a && (a+b>c && b+c>a && a+c>b) && (a>0 && b>0 && c>0)){
+            return TAMGIACTHUONG;
+        }
+        if((a+b<c || b+c<a || a+c<b) && (a<=0 || b<=0 || c<=0)){
+            return NOTATRIANGLE;
+        }
+        return NOTATRIANGLE;
+    }
+
+
+
 }
--- src/triangle/TriangleClassifierTest.java
+++ src/triangle/TriangleClassifierTest.java
@@ -1,4 +1,53 @@
+package triangle;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static triangle.TriangleClassifier.*;
+
 public class TriangleClassifierTest {
-  
+
+
+    @Test
+    public void classify222() {
+        String result = TriangleClassifier.classify(2,2,2);
+        assertEquals(TriangleClassifier.TAM_GIAC_DEU, result);
+    }
+    @Test
+    public void classify223() {
+        String result = TriangleClassifier.classify(2,2,3);
+        assertEquals(TAMGIACCAN, result);
+    }
+    @Test
+    public void classify232() {
+        String result = TriangleClassifier.classify(2,3,2);
+        assertEquals(TAMGIACCAN, result);
+    }
+    @Test
+    public void classify233() {
+        String result = TriangleClassifier.classify(2,3,3);
+        assertEquals(TAMGIACCAN, result);
+    }
+    @Test
+    public void classify345() {
+        String result = TriangleClassifier.classify(3,4,5);
+        assertEquals(TAMGIACTHUONG, result);
+    }
+    @Test
+    public void classify823() {
+        String result = TriangleClassifier.classify(8,2,3);
+        assertEquals(NOTATRIANGLE, result);
+    }
+    @Test
+    public void classifyminus12minus1(){
+        String result = TriangleClassifier.classify(-1,2,-1);
+        assertEquals(NOTATRIANGLE, result);
+    }
+    @Test
+    public void classify011(){
+        String result = TriangleClassifier.classify(0,1,1);
+        assertEquals(NOTATRIANGLE, result);
+    }
+
+
 }
